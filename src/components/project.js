a02504643cb71803f03bd8767f15de82c9cdb973
Santosh Yadav <ymanoutube@gmail.com>
2023-12-11 23:08:59 +0545

    Project Section Added

---
+++ src/components/project.js
@@ -0,0 +1,17 @@
+const project = [
+    {
+        name:"Crowdfunding",
+        description:" A Blockchain based open source crowdfunding platform."
+    },
+    {
+        name:"Shoping Cart",
+        description:" A shopping Cart made using typescript and bootstrap"
+    },
+    {
+        name:"LinkedUs",
+        description:" A Job Portal made using MERN stack"
+    }
+    
+]
+
+export default project;
